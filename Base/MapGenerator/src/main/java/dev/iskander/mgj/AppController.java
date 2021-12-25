f20801bcc0ad5a5e94824c603a220f2d218e977d
Abanoub Iskander <abanoub@iskander.dev>
2021-12-25 21:55:10 +0000

    now outputs spikey circles when you click the button

--- Base/MapGenerator/src/main/java/dev/iskander/mgj/AppController.java
+++ Base/MapGenerator/src/main/java/dev/iskander/mgj/AppController.java
@@ -2,7 +2,9 @@ package dev.iskander.mgj;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
+import java.util.Random;
 
 public class AppController {
 
@@ -10,12 +12,9 @@ public class AppController {
 	Canvas canvas;
 
 	@FXML
-	ColorPicker colourPicker;
-
-	@FXML
 	void doTheThing() {
-
-		CanvasDrawifier.drawPath(canvas.getGraphicsContext2D(), colourPicker.getValue());
+		Random random = new Random();
+		CanvasDrawifier.drawBiome(canvas.getGraphicsContext2D(), new Color(random.nextDouble(), random.nextDouble(), random.nextDouble(), 0.5), random.nextDouble(100));
 
 	}
 
--- Base/MapGenerator/src/main/java/dev/iskander/mgj/CanvasDrawifier.java
+++ Base/MapGenerator/src/main/java/dev/iskander/mgj/CanvasDrawifier.java
@@ -4,6 +4,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import java.util.Random;
+
+
 public class CanvasDrawifier {
 
 	public static void drawShapes(GraphicsContext gc) {
@@ -21,15 +24,64 @@ public class CanvasDrawifier {
 
 	}
 
-	public static void drawPath(GraphicsContext gc, Color colour) {
+	public static void drawBiome(GraphicsContext gc, Color colour, double maxSize) {
 
-		System.out.println(colour);
+		Random random = new Random();
 
 		gc.setStroke(colour);
 		gc.setFill(colour);
 
-		//TODO implement a way for a random path to be drawn
+		final double startX = random.nextDouble(gc.getCanvas().getWidth());
+		final double startY = random.nextDouble(gc.getCanvas().getHeight());
 
+		for (int ii = 0; ii < 1_000; ii++) {
+			drawFromPoint(gc, startX, startY, maxSize);
+		}
+
+	}
+
+	static double[] drawFromPoint(GraphicsContext gc, double startX, double startY, double maxSize) {
+
+		gc.beginPath();
+		gc.moveTo(startX, startY);
+		double[] endpoints = findEndpoint(gc, startX, startY, maxSize);
+		System.out.println(endpoints[0] + " " + endpoints[1]);
+		gc.lineTo(endpoints[0], endpoints[1]);
+		gc.stroke();
+		gc.closePath();
+		return endpoints;
+	}
+
+	private static double[] findEndpoint(GraphicsContext gc, double startX, double startY, double maxSize) {
+		Random random = new Random();
+		return validateEndpoints(gc, random, startX, startY, maxSize);
+
+	}
+
+	private static double[] validateEndpoints(GraphicsContext gc, Random random, double startX, double startY, double maxSize) {
+		double endX = random.nextDouble(Math.sqrt(maxSize));
+		double endY = random.nextDouble(Math.sqrt(maxSize));
+		System.out.println(endX + " " + endY);
+		if ((endX * endX) + (endY * endY) > maxSize) {
+			return validateEndpoints(gc, random, startX, startY, maxSize); // max length
+		}
+		if (endX < 0 || endX > gc.getCanvas().getWidth()) {
+			return validateEndpoints(gc, random, startX, startY, maxSize); // stay in bounds
+		}
+		if (endY < 0 || endY > gc.getCanvas().getHeight()) {
+			return validateEndpoints(gc, random, startX, startY, maxSize);
+		}
+
+		if (random.nextBoolean()) {
+			endX = -endX;
+		}
+		if (random.nextBoolean()) {
+			endY = -endY;
+		}
+		endX += startX;
+		endY += startY;
+		System.out.println(endX + "_________________________" + endY);
+		return new double[]{endX, endY};
 	}
 
 	private static Color parseColour(String colourString) {
